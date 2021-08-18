31fbb7e197707aeabb11df6e323fc6b762b3dad6
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-18 17:11:47 -0300

    adding extra icons, name, email and avatar to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header(){
 	return (
@@ -23,7 +23,7 @@ export function Header(){
 				<Text as="span" ml="1" color="pink.500">.</Text>
 			</Text>
 
-			{/* since we will have an icon and an input the best way
+			{/* since we will have an icon and an input, the best way
 			    is to make the element that wraps them looks like an input
 					to users click. The div is a label because if the persons clicks
 					in the icon the input will also receive focus  */}
@@ -50,6 +50,45 @@ export function Header(){
 				/>
 				<Icon as={RiSearchLine} fontSize="20" />
 			</Flex>
+			
+			<Flex
+				align="center"
+				ml="auto" // places in the extreme right
+			>
+
+				<HStack 
+					spacing="8"
+					mx="8"
+					pr="8"
+					py="1"
+					color="gray.300"
+					borderRightWidth={1}
+					borderColor="gray.700"
+				>
+					<Icon as={RiNotificationLine} fontSize="20" />
+					<Icon as={RiUserAddLine} fontSize="20" />
+				</HStack>
+
+				<Flex align="center">
+					<Box mr="4" textAlign="right">
+						<Text>Bruno Toresan</Text>
+						<Text 
+							color="gray.300" 
+							fontSize="small"
+						>
+							brunotoresan@gmail.com
+						</Text>
+					</Box>
+				</Flex>
+
+				<Avatar
+					size="md"
+					name="Bruno Toresan"
+					src="https://github.com/brunotoresan.png"
+				/>
+
+			</Flex>
+
 		</Flex>
 	)
 }
