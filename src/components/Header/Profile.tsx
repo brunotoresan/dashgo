8bb5c5414827694771371f36528e4f89a3f9ec4f
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-20 18:22:39 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData ?: boolean
+}
+
+export function Profile({ showProfileData = true }) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Bruno Toresan</Text>
-        <Text color="gray.300" fontSize="small">
-          brunotoresan@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Bruno Toresan</Text>
+          <Text color="gray.300" fontSize="small">
+            brunotoresan@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Bruno Toresan" src="https://github.com/brunotoresan.png" />
     </Flex>
