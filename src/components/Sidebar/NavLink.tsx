137e2fe2edc9fbd6fb461a5640010c8d8af287b8
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-30 18:01:07 -0300

    deleting unused import

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,5 @@
 import { Text, Link as ChakraLink, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from 'next/link'
 import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
